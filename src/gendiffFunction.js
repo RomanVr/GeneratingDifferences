bc0403f6b8081613099d4713ee58e25a5a623f18
Roman Vr <romakf99@yandex.ru>
2018-09-20 22:39:39 +0300

    added parsing changes with the object

--- src/gendiffFunction.js
+++ src/gendiffFunction.js
@@ -2,11 +2,18 @@ import _ from 'lodash';
 import fs from 'fs';
 import parse from './parsers';
 
+const getExtentionFile = (strPath) => {
+  const pathToStrArray = strPath.split('.');
+  return pathToStrArray[pathToStrArray.length - 1];
+};
+
 const gendiffFunction = (pathToFile1, pathToFile2) => {
   const fileBefore = fs.readFileSync(pathToFile1, 'utf8');
   const fileAfter = fs.readFileSync(pathToFile2, 'utf8');
-  const beforeObj = parse(fileBefore);
-  const afterObj = parse(fileAfter);
+  const extentionFileBefore = getExtentionFile(pathToFile1);
+  const extentionFileAfter = getExtentionFile(pathToFile2);
+  const beforeObj = parse({ extentionFile: extentionFileBefore, data: fileBefore });
+  const afterObj = parse({ extentionFile: extentionFileAfter, data: fileAfter });
   const keys = _.union(_.keys(beforeObj), _.keys(afterObj));
   const reducer = (acc, key) => {
     if (_.has(beforeObj, key) && !_.has(afterObj, key)) return [...acc, `  - ${key}: ${beforeObj[key]}\n`];
--- src/parsers.js
+++ src/parsers.js
@@ -1,31 +1,26 @@
 import yaml from 'js-yaml';
-import _ from 'lodash';
 
-const parseJson = data => JSON.parse(data);
+const parseJson = ({ data }) => JSON.parse(data);
 
-const parseYaml = data => yaml.safeLoad(data);
+const parseYaml = ({ data }) => yaml.safeLoad(data);
 
 const mappingToParse = [
   {
-    parseIt: parseJson,
-    check: (arg) => {
-      try {
-        JSON.parse(arg);
-        return true;
-      } catch (e) {
-        return false;
-      }
-    },
+    parseIt: parseYaml,
+    check: arg => arg === 'yml',
   },
   {
-    parseIt: parseYaml,
-    check: arg => _.isObject(yaml.safeLoad(arg)),
+    parseIt: parseJson,
+    check: arg => arg === 'json',
   },
 ];
-const getParseAction = arg => mappingToParse.find(({ check }) => check(arg));
-const parse = (strData) => {
-  const { parseIt } = getParseAction(strData);
-  return parseIt(strData);
+
+const getParseAction = ({ extentionFile }) => mappingToParse
+  .find(({ check }) => check(extentionFile));
+
+const parse = (objData) => {
+  const { parseIt } = getParseAction(objData);
+  return parseIt(objData);
 };
 
 export default parse;
