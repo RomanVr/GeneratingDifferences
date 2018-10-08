c1ba609ac501b0f8783eba6341b8d63dc17c1595
Roman Vr <romakf99@yandex.ru>
2018-10-08 23:16:42 +0300

    correction of the some remarks after third satge

--- src/getAstDiff.js
+++ src/getAstDiff.js
@@ -1,26 +1,17 @@
 import _ from 'lodash';
 
 const getAstDiff = (beforeObj, afterObj) => {
-  const iter = (bObj, aObj, astAcc) => {
-    const keys = _.union(_.keys(bObj), _.keys(aObj));
-
-    const reducer = (acc, key) => {
-      // Ключ только у beforeObj
-      if (_.has(bObj, key) && !_.has(aObj, key)) return [...acc, { nameProperty: key, statusProperty: 'deleted', dataProperty: bObj[key] }];
-      // Ключ только у afterObj
-      if (!_.has(bObj, key) && _.has(aObj, key)) return [...acc, { nameProperty: key, statusProperty: 'added', dataProperty: aObj[key] }];
-      // Если значение ключа типа Object, одинаковые у обоих
-      if (_.isPlainObject(bObj[key]) && _.isPlainObject(aObj[key])) return [...acc, { nameProperty: key, statusProperty: 'unchanged', dataProperty: iter(bObj[key], aObj[key], []) }];
-      // Если значение ключа у всех типа string
-      if (bObj[key] === aObj[key]) return [...acc, { nameProperty: key, statusProperty: 'unchanged', dataProperty: bObj[key] }];
-      // В остальных случаях у свойства изменились данные
-      return [...acc, {
-        nameProperty: key, statusProperty: 'changed', beforeProperty: bObj[key], afterProperty: aObj[key],
-      }];
-    };
-    return keys.reduce(reducer, astAcc);
+  const keys = _.union(_.keys(beforeObj), _.keys(afterObj));
+  const reducer = (acc, key) => {
+    if (_.has(beforeObj, key) && !_.has(afterObj, key)) return [...acc, { nameProperty: key, statusProperty: 'deleted', dataProperty: beforeObj[key] }];
+    if (!_.has(beforeObj, key) && _.has(afterObj, key)) return [...acc, { nameProperty: key, statusProperty: 'added', dataProperty: afterObj[key] }];
+    if (_.isPlainObject(beforeObj[key]) && _.isPlainObject(afterObj[key])) return [...acc, { nameProperty: key, statusProperty: 'unchanged', dataProperty: getAstDiff(beforeObj[key], afterObj[key], []) }];
+    if (beforeObj[key] === afterObj[key]) return [...acc, { nameProperty: key, statusProperty: 'unchanged', dataProperty: beforeObj[key] }];
+    return [...acc, {
+      nameProperty: key, statusProperty: 'changed', beforeProperty: beforeObj[key], afterProperty: afterObj[key],
+    }];
   };
-  return iter(beforeObj, afterObj, []);
+  return keys.reduce(reducer, []);
 };
 
 export default getAstDiff;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import gendiff from './gendiffFunction';
+import fs from 'fs';
+import path from 'path';
+import getAstdiff from './getAstDiff';
+import render from './render';
+import parse from './parsers';
 
-export default gendiff;
+const gendiffProperties = (path1, path2) => {
+  const dataBefore = fs.readFileSync(path1, 'utf8');
+  const dataAfter = fs.readFileSync(path2, 'utf8');
+  const typeDataBefore = path.extname(path1).substring(1);
+  const typeDataAfter = path.extname(path2).substring(1);
+  const beforeObj = parse(typeDataBefore, dataBefore);
+  const afterObj = parse(typeDataAfter, dataAfter);
+  const astDiff = getAstdiff(beforeObj, afterObj);
+  return render(astDiff);
+};
+
+export default gendiffProperties;
