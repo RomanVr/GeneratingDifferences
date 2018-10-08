a9c8d2428b602e691055783638821e86ba06772b
Roman Vr <romakf99@yandex.ru>
2018-10-08 15:08:10 +0300

    change version project to 2.0.1

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,7 +2,7 @@
 import program from 'commander';
 
 program
-  .version('2.0.0')
+  .version('2.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>');
