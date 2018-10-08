1a44e00c6f4f6a6f84859b35287603d35aaa5be7
Roman Vr <romakf99@yandex.ru>
2018-10-08 15:05:21 +0300

    added arguments in program CLI

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,7 @@ import program from 'commander';
 program
   .version('2.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format');
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>');
 
 program.parse(process.argv);
